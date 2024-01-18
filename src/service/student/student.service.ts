2b0734054ca0cc572c774049ec2d998579819b03
Anselmo <37450885+AnselmoAlvesJunior@users.noreply.github.com>
2024-01-17 21:00:34 -0300

    type fixed

--- src/service/student/student.service.ts
+++ src/service/student/student.service.ts
@@ -40,7 +40,8 @@ export class StudentService {
     return existingStudent;
   }
   async deleteStudent(studentId: string): Promise<IStudent> {
-    const deletedStudent = await this.studentModel.findByIdAndDelete(studentId);
+    const deletedStudent = await this.studentModel.findByIdAndDelete(studentId) as unknown as IStudent;
+
     if (!deletedStudent) {
       throw new NotFoundException(`Student #${studentId} not found`);
     }
